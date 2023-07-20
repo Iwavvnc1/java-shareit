db8fa6d3fb2ad1a1583b62893a735f894afa945f
Iwavvnc1 <popigbang@yandex.ru>
2023-07-20 13:52:17 +0500

    TZ15 add unit test

---
+++ src/test/java/ru/practicum/shareit/user/storage/UserRepositoryTest.java
@@ -0,0 +1,25 @@
+package ru.practicum.shareit.user.storage;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import ru.practicum.shareit.user.model.User;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+@DataJpaTest
+class UserRepositoryTest {
+@Autowired
+private UserRepository userRepository;
+
+@BeforeEach
+public void addUser() {
+    userRepository.save(new User(1L,"name","mail@mail.com"));
+}
+
+    @Test
+    void existsUserById() {
+    Long userId = 1L;
+        assertTrue(userRepository.existsUserById(userId));
+    }
+}
